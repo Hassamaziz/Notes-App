55b931ab42fa84c01cf0be7d27089162bb0e131a
hassamaziz <hassamaziz001@gmail.com>
2025-07-21 17:09:07 +0500

    Routes

---
+++ backend/routes/notesRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const router = express.Router();
+
+// app.get('/api/notes', (req, res) => {
+//   res.status(200).send('you got 50000 notes');
+// });
+
+// app.post('/api/notes', (req, res) => {
+//   res.status(201).send('Note created successfully');
+// });
+
+// app.put('/api/notes/:id', (req, res) => {
+//   res.status(200).send('Note updated successfully');
+// });
+// app.delete('/api/notes', (req, res) => {
+//   res.status(200).send('Note deleted successfully');
+// });
+
+
+export default router;

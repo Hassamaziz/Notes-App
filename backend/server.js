148c1f004f2b82c7b6add4d9144f3d424bda5db7
hassamaziz <hassamaziz001@gmail.com>
2025-07-21 17:01:53 +0500

    /

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,17 @@ app.get('/api/notes', (req, res) => {
   res.status(200).send('you got 50000 notes');
 });
 
+app.post('/api/notes', (req, res) => {
+  res.status(201).send('Note created successfully');
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  res.status(200).send('Note updated successfully');
+});
+app.delete('/api/notes', (req, res) => {
+  res.status(200).send('Note deleted successfully');
+});
+
 app.listen(5001, () => {
   console.log('Server is running on port 5001');
 });

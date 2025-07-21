b145d60d22019bfbf927c8bfdae8078d3efeb937
hassamaziz <hassamaziz001@gmail.com>
2025-07-22 03:12:46 +0500

    working on middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5001;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+app.use((req, res, next)=>{
+  console.log("we just got a request");
+  next();
+})
 
 
 connectDB();

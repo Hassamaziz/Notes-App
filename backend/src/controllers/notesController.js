8a7271cd4e15a18dbea7502b32630baf92148860
hassamaziz <hassamaziz001@gmail.com>
2025-07-21 21:13:45 +0500

    controller workings

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,5 +1,15 @@
- export function getAllNotes (req, res) {
-  res.status(200).send("You just fetched the notes successfully");
+ 
+import Note from '../models/Note.js';
+
+ export async function getAllNotes (req, res) {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+    
+  } catch (error) {
+    console.error("Error fetching notes:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export function createNote (req, res) {
--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 
 const noteSchema = new mongoose.Schema({
-  title: {typeof: String, required: true},
-  content: {type: String, required: true}
+  title: {type: String, required: true},
+  content: {type: String, required: true},
 }, {
   timestamps: true
 });

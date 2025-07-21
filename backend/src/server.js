3ac0a1ab5b00c5441c8cc57382f202d3d5bc5996
hassamaziz <hassamaziz001@gmail.com>
2025-07-21 18:05:47 +0500

    now working on Model

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { connectDB } from "../config/db.js";
 
+
 const app = express();
+
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
 });
 
 //  database password   5xEUC4JRRk7ENuPl

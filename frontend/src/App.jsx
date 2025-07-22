bf460f7cde60cfb36f8983d9630c6b2b425ac34e
hassamaziz <hassamaziz001@gmail.com>
2025-07-22 19:18:55 +0500

    DaisyUI theme added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage'
-import NoteDetailPage from './pages/NoteDetailPage'
-import CreatePage from './pages/CreatePage'
-import toast from 'react-hot-toast'
+import React from "react";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import CreatePage from "./pages/CreatePage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>
-      <button className='btn btn-outline'>Click Here</button>
-        <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/create" element={<CreatePage />} />
-      <Route path="/note/:id" element={<NoteDetailPage />} />
-      
-    </Routes>
-
+    <div data-theme="forest">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

6d2c92208c76fa098bc7362b32f7a6c9685da618
hassamaziz <hassamaziz001@gmail.com>
2025-07-28 20:01:17 +0500

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import toast from "react-hot-toast";
 const App = () => {
   return (
     <div data-theme="forest">
+      {/* Routes */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />

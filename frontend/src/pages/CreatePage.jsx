67c34897dbc3264ddaab63193474deb0464d0343
hassamaziz <hassamaziz001@gmail.com>
2025-07-31 19:05:19 +0500

    create page working starts

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const CreatePage = () => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [loading, setLoading] = useState(false)
+
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+
+  }
+
   return (
-    <div>CreatePage</div>
+    <div className='min-h-screen bg-base-200'>
+      <div className='container mx-auto px-4 py-8'>
+
+      </div>
+    </div>
   )
 }
 

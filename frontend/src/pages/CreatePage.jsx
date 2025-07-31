a06e0b3740282d37dc1d20042c74ab11093e6045
hassamaziz <hassamaziz001@gmail.com>
2025-07-31 19:24:03 +0500

    ..

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { Link } from 'react-router';
+import { ArrowLeftIcon } from 'lucide-react';
+import { useState } from 'react';
+
 
 const CreatePage = () => {
   const [title, setTitle] = useState('')
@@ -14,7 +17,12 @@ const CreatePage = () => {
   return (
     <div className='min-h-screen bg-base-200'>
       <div className='container mx-auto px-4 py-8'>
-
+        <div className='max-w-2xl mx-auto'>
+         <Link to={"/"} className='btn btn-ghost mb-6'>
+         <ArrowLeftIcon className='size-5' />
+          Back to Notes
+         </Link>
+        </div>
       </div>
     </div>
   )
